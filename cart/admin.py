5548c9d59ddedd4478e8ca18cd4e889243216175
Mohammad Sharif Shokouhi <m.sh.shokouhi@outlook.com>
2025-05-18 12:49:39 +0330

    add cart models to admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Cart, CartItem
 
-admin.site.register(Cart)
-admin.site.register(CartItem)
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user','id','status',)
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity')
